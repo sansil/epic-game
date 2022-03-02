8060ff2091de7f0e3e0c3ccd31cfc48f7fce5b7d
sansil <sansil32@gmail.com>
2022-03-01 22:33:16 -0300

    init

---
+++ scripts/run.js
@@ -0,0 +1,50 @@
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+  const gameContract = await gameContractFactory.deploy(
+    ["Leo", "Aang", "Pikachu"],
+    ["https://i.imgur.com/pKd5Sdk.png",
+      "https://i.imgur.com/xVu4vFL.png",
+      "https://i.imgur.com/u7T87A6.png"],
+    [100, 200, 300],
+    [100, 50, 25],
+    "Elon Musk", // Boss name
+    "https://i.imgur.com/AksR0tt.png", // Boss image
+    10000, // Boss hp
+    50 // Boss attack damage
+  );
+  await gameContract.deployed();
+  console.log("Contract deployed to:", gameContract.address);
+  let txn;
+  // We only have three characters.
+  // an NFT w/ the character at index 2 of our array.
+  txn = await gameContract.mintCharacterNFT(2);
+  await txn.wait();
+
+  // Get the value of the NFT's URI.
+  let returnedTokenUri = await gameContract.tokenURI(1);
+  console.log("Token URI:", returnedTokenUri);
+
+
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
